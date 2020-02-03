3d515904676c06aa322757e3dad5d3f5a533ca22
Runo-saduwa <orunosaduwa@gmail.com>
2020-02-03 20:13:15 +0100

    create user test

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,8 +2,33 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server/app';
 import { org1, org2 } from './mockData/organization.mock';
+import {user1, user2} from './mockData/user.mock';
 chai.use(chaiHttp);
 
+describe('User Register Test', () => {
+  it('should return a 200 status on successful registration', async () => {
+    const { body, status } = await chai
+      .request(server)
+      .post('/api/auth/user/register/')
+      .send(user1);
+    expect(status).to.eql(201);
+    expect(body).to.haveOwnProperty('token');
+  });
+
+  it('should return an error when email exists', async () => {
+    const {
+      body: { message },
+      status,
+    } = await chai
+      .request(server)
+      .post('/api/auth/user/register/')
+      .send(user2);
+    expect(status).to.eql(400);
+    expect(message).to.eql('Email already exists');
+  });
+
+
+})
 describe('Organization Register Tests', () => {
   it('should return a 200 status on successful registration', async () => {
     const { body, status } = await chai
---
+++ src/tests/mockData/user.mock.ts
@@ -0,0 +1,20 @@
+
+interface User {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+}
+
+export const user1: User = {
+  name: 'Eren Jaeger',
+  email: 'eren@gmail.com',
+  username: 'eren',
+  password: 'password'
+};
+export const user2: User = {
+  name: 'Eren Jaeger',
+  email: 'eren@gmail.com',
+  username: 'eren',
+  password: 'password'
+};
