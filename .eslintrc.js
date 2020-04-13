92601561080d6bbd45f005cf8f271497d6376352
Runo-saduwa <orunosaduwa@gmail.com>
2020-04-13 13:22:56 +0100

    chore(migrate-to-nestjs): Migrate codebase to NestJS
    - Installs NestJS, its dependencies and boilerplates
    - Deletes conflicting files
    - Updates test command in .circleci
    - Updates test script to also run give test coverage reports
    - Adds new folders to the src directories - validators, constants, controllers, routes, repositories, helpers, models
    - Updates .gitignore
    - ensures all tests pass and coverage reports comes out successfully
    - uninstall dormant dependencies
    - Created varius modules for different parts of the application

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,24 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ],
-  ignorePatterns: ["src/database"],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
   },
   rules: {
-
-  }
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
