d2a864d4a4b67a9623ecb4591da6904e9f88acc6
Runo-saduwa <orunosaduwa@gmail.com>
2020-04-16 11:54:44 +0100

    chore(migrate-to-typeorm): Migrate from sequelize to typeorm
    - Installs typeorm, typeorm/nest and pg
    - Imports ormconfig into app module and adds it to imports array
    - writes entities for user, role, membership and organization
    - generates migrations
    - writes ormconfig file properly
    - writes env variables in envsample
    - deleted comments and added verified field

---
+++ src/organization/organization.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({name: 'Organization'})
+export class Organization {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  logoURL: string;
+
+  @Column()
+  foundIn: Date;
+
+  @Column()
+  industry: string;
+
+  @Column()
+  description: string
+
+  @Column()
+  contactPhone: string
+
+  @Column({
+    default: false
+  })
+  verified: boolean;
+}
